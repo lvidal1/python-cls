332d7688bb680bf0a8d0c38e007bd79871a7e22b
lvidal1 <lvidal910@gmail.com>
2023-06-21 00:34:04 -0500

    Add notification model

---
+++ frontend/models/src/notification.ts
@@ -0,0 +1,40 @@
+import BaseModel from './base-model'
+import { defaultObject, defaultString , defaultBoolean } from './defaults'
+
+export type NotificationDataType = {
+  id: string | null | undefined;
+  created_at: string | null | undefined;
+  seen: boolean;
+  type: keyof NotificationTypeValues | null | undefined;
+};
+
+type NotificationTypeValues = {
+  welcome: string;
+  alert: string;
+};
+
+export default class Notification extends BaseModel {
+  id: string
+
+  created_at: string
+
+  seen: boolean
+
+  type: string
+
+  constructor(
+    data: NotificationDataType = {
+      created_at: null,
+      seen: false,
+      type: null,
+      id: null,
+    },
+  ) {
+    super()
+    this.id = defaultString(data.id)
+    this.type = defaultString(data.type)
+    this.seen = defaultBoolean(data.seen)
+    this.created_at = defaultObject(data.created_at)
+  }
+
+}
