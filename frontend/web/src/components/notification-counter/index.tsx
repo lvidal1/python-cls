bbcc4143441501ea5a6a0ba80e16d8a48fa5c25f
lvidal1 <lvidal910@gmail.com>
2023-06-24 13:24:15 -0500

    Add notification selector

--- frontend/web/src/components/notification-counter/index.tsx
+++ frontend/web/src/components/notification-counter/index.tsx
@@ -1,55 +1,42 @@
 import React, { useEffect } from 'react'
 import { ApiPayload, reduxSet as apiAC, } from '@clearsummit/radio-dispatch'
+import { Notification, } from '@challenge/models'
+
 import { connect, } from 'react-redux'
 import { Dispatch, } from 'redux'
-
-import services, { endpoints, } from '@/helpers/services'
+import services from '@/helpers/services'
+import { StoreState, } from '@/redux'
 import { getNotificationPayload, } from '@/redux/api-payloads'
+import selectors from '@/selectors'
 
 import styles from './styles.scss'
-import { makeRequest } from '@clearsummit/radio-dispatch/lib/api-redux'
 
+interface NotificationProps {
+  notifications: Notification[]
+}
 
 interface DispatchToProps {
-  /** Dispatch request payload */
   makeRequest: (payload: ApiPayload<typeof services>) => void,
 }
 
-type Props = DispatchToProps
+type Props = NotificationProps & DispatchToProps
 
 const NotificationCounter = (props: Props) => {
-  const { makeRequest, } = props
+  const { makeRequest, notifications } = props
 
   useEffect(() => {
     const payload = getNotificationPayload({ count: 3 })
     makeRequest(payload)
-  })
-
-  const notifications: any = [{
-    "id": "3",
-    "created_at": "2022-01-14T21:38:19.948287Z",
-    "seen": false,
-    "type": "alert"
-  },
-  {
-    "id": "2",
-    "created_at": "2022-01-14T21:38:02.821390Z",
-    "seen": false,
-    "type": "alert"
-  },
-  {
-    "id": "1",
-    "created_at": "2022-01-14T21:37:56.138788Z",
-    "seen": false,
-    "type": "welcome"
-  }]
+  }, [])
 
   return (
-    <div className={styles.button}><span className={styles.badge}>{notifications?.length ?? null}</span></div>
+    <div className={styles.button}><span className={styles.badge}>{notifications.length ?? null}</span></div>
   )
 }
 
-const mapStateToProps = () => ({})
+const mapStateToProps = (state: StoreState) => ({
+  notifications: selectors.user.getNotifications(state),
+})
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   makeRequest: (payload: ApiPayload<typeof services, unknown>) => dispatch(apiAC.makeRequest.dispatch(payload)),
